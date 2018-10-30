c0834fef40f6bd812987b6dfdaeebc0f69a4098a
njriasan <njriasanovsky@berkeley.edu>
2018-10-30 16:48:56 -0700

    added map functionality without SQL yet

--- user_map/user_map.go
+++ user_map/user_map.go
@@ -1,21 +1,43 @@
 type MapKey struct {
-	TreeId int64
+	LogId int64
 	UserId string
 	PublicKey string
 }
 
 type UserData struct {
+	LogId int64
 	UserId string
 	PublicKey string
 	UserIdentifier string
 }
 
-type UserLeafData struct {
-	PublicKey string
-	UserIdentifier string
+/*
+	Composes the MapKey from the UserData it seeks to update and the id
+	for the tree being used.
+*/
+func CreateMapKey (data *UserData) *MapKey {
+	return &{Logid: data->LogId, UserId: data->UserId, PublicKey: data->PublicKey}
 }
 
-func CreateMapKey () {
+/*
+	Makes a SQL query to the database to acquire all nodes that will need
+	to be updated for the user.
+*/
+func searchMap (ctx context.Context, client *trillian.TrillianLogClient, key *MapKey) []*UserData, error {
+}
+
+/*
+	Deletes all entries with the associated Key from the map.
+*/
+func deleteFromMap (ctx context.Context, client *trillian.TrillianLogClient, key *Mapkey) error {
+
+}
+
+/*
+	Adds new entries to the map for each UserData value and the
+	newPk.
+*/
+func addToMap (ctx context.Context, client *trillian.TrillianLogClient, nodes []*UserData, newPk string) error {
 
 }
 
@@ -24,5 +46,21 @@ func CreateMapKey () {
 	UserData nodes that will need to be updated and errors if no map
 	exists.
 */
-func GatherNodes (datum *UserData) []*UserData, error  {
+func GatherNodes (ctx context.Context, client *trillian.TrillianLogClient, datum *UserData) []*UserData, error {
+	mapKey := CreateMapKey (datum)
+	return searchMap (ctx, client, mapkey)
+}
+
+/*
+	Function to update the Map in the database. It first adds the new nodes
+	and then deletes the previous ones.
+*/
+func updateMap (ctx context.Context, client *trillian.TrillianLogClient, nodes []*UserData, newPk string) error {
+	err := addToMap (ctx, client, nodes, newPk)
+	if err != nil {
+		return err
+	}
+	mapKey := CreateMapKey (nodes[0])
+	err = deleteFromMap (ctx, client, mapKey)
+	return err
 }
